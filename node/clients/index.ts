fc10bbce18a8373e1d391e703ac74f20cdc5a16b
Bruno Moreira <bruno@brunormoreira.com>
2021-07-07 15:57:01 -0300

    Add ExternalProvider Client

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,15 @@
 import { IOClients } from '@vtex/api'
 
 import Checkout from './checkout'
+import ExternalProvider from './externalProvider'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get checkout() {
     return this.getOrSet('checkout', Checkout)
+  }
+
+  public get externalProvider() {
+    return this.getOrSet('externalProvider', ExternalProvider)
   }
 }
