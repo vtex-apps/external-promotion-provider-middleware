f2bdbe22cabd84e2084df36fe6b74459f5e8808a
Pedro Cruz <pscruzzz@outlook.com>
2021-07-09 00:16:19 -0300

    feat: add event to listen to settings update
    This commit adds an apps event that notifies when settings has been updated

--- node/middlewares/setupAppConfiguration.ts
+++ node/middlewares/setupAppConfiguration.ts
@@ -2,6 +2,8 @@ import type { OrderFormConfiguration } from '@vtex/clients'
 
 import orderFormConfigurationService from '../services/orderFormConfiguration'
 
+type IAppSettings = { externalEndpoint: string }
+
 const setupAppConfiguration = async (
   ctx: InstalledAppEvent,
   next: () => Promise<any>
@@ -12,6 +14,23 @@ const setupAppConfiguration = async (
   orderFormConfigurationService.setConfiguration({
     client: checkout,
     configuration: {} as OrderFormConfiguration, // fixes ts error
+  })
+
+  const eventSender = ctx.body?.id ?? 'Sender was not provided'
+
+  const appSettings: IAppSettings = await ctx.clients.apps.getAppSettings(
+    process.env.VTEX_APP_ID as string
+  )
+
+  ctx.state.appSettings = appSettings
+
+  ctx.vtex.logger.debug({
+    eventState: {
+      status: 'received',
+      sender: eventSender,
+      event: ctx.vtex.eventInfo,
+      content: ctx.state.appSettings,
+    },
   })
 
   await next()
