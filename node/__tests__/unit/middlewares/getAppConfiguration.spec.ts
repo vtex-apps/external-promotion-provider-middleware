692ec03a9d4e7834f97da0823d6250bf4b4a46d1
Pedro Cruz <pscruzzz@outlook.com>
2021-07-14 00:40:25 -0300

    refactor: refact test to track settings instead of state

--- node/__tests__/unit/middlewares/getAppConfiguration.spec.ts
+++ node/__tests__/unit/middlewares/getAppConfiguration.spec.ts
@@ -2,11 +2,10 @@ import { getAppConfiguration } from '../../../middlewares'
 
 describe('getAppConfiguration', () => {
   const ctx = {
-    state: {},
     clients: {
       apps: { getAppSettings: jest.fn() },
     },
-    vtex: { logger: { error: jest.fn() } },
+    vtex: { logger: { error: jest.fn() }, settings: {} },
   } as any
 
   it('should insert the app settings into context', async () => {
@@ -19,9 +18,9 @@ describe('getAppConfiguration', () => {
 
     await getAppConfiguration(ctx, next)
     expect(spyGetAppSettings).toHaveBeenCalled()
-    expect(ctx.state.appSettings).toEqual({
-      externalEndpoint: 'http://localhost:3000/api',
-    })
+    expect(ctx.vtex.settings.externalEndpoint).toEqual(
+      'http://localhost:3000/api'
+    )
   })
   it('should return error when getAppSettings is rejected', async () => {
     jest
