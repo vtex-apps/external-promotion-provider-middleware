c536d84893d39894c2daa4298fb934d2f987a310
Pedro Cruz <pscruzzz@outlook.com>
2021-07-14 00:32:42 -0300

    feat: add external endpoint as base url in externalProvider super

--- node/clients/externalProvider.ts
+++ node/clients/externalProvider.ts
@@ -4,7 +4,7 @@ import { ExternalClient } from '@vtex/api'
 export default class ExternalProvider extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
     // TODO: Validate how to add an outbound policy dynamically to hosts
-    super(context.settings.url, context, options)
+    super(context.settings.externalEndpoint, context, options)
   }
 
   /* eslint-disable @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unused-vars */
