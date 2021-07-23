d5b2b80e6d3ad1121283cefbb743d614578ad007
Pedro Cruz <pscruzzz@outlook.com>
2021-07-23 19:37:09 -0300

    test: test applyApportionment

---
+++ node/__mocks__/externalProviderResponse.ts
@@ -0,0 +1,78 @@
+export const externalProviderResponse = {
+  items: [
+    {
+      id: 33,
+      variations: [
+        {
+          requestIndex: 0,
+          quantity: 2,
+          externalPromotions: [
+            {
+              matchedParameters: {
+                slaIds: 'transportadora genérica',
+              },
+              identifier: '3f0c2115-10a6-4fda-aec4-5950d8de9a6c',
+              isPercentual: false,
+              value: -700,
+            },
+          ],
+        },
+        {
+          requestIndex: 1,
+          quantity: 2,
+          externalPromotions: [
+            {
+              matchedParameters: {
+                slaIds: 'transportadora genérica',
+              },
+              identifier: '3f0c2115-10a6-4fda-aec4-5950d8de9a6c',
+              isPercentual: false,
+              value: -100,
+            },
+            {
+              matchedParameters: {
+                slaIds: 'transportadora genérica',
+              },
+              identifier: '3f0c2115-10a6-4fda-aec4-5950d8de9a6c',
+              isPercentual: true,
+              value: 0.2,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 30,
+      variations: [
+        {
+          requestIndex: 3,
+          quantity: 1,
+          externalPromotions: [
+            {
+              matchedParameters: {
+                dockId: '2343b',
+              },
+              identifier: '3f0c2115-10a6-4fda-aec4-5950d8de9a6c',
+              isPercentual: false,
+              value: -100,
+            },
+          ],
+        },
+        {
+          requestIndex: 5,
+          quantity: 1,
+          externalPromotions: [
+            {
+              matchedParameters: {
+                skuName: 'SKU 23 - Celular',
+              },
+              identifier: '3f0c2115-10a6-4fda-aec4-5950d8de9a6c',
+              isPercentual: false,
+              value: -500,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
