01638c6399fdb8afe5656ec9f8020313915718d6
Pedro Cruz <pscruzzz@outlook.com>
2021-07-26 19:00:13 -0300

    feat: add error handling to calculateExternalBenefits middleware
    This commit handles errors inside calculateExternalBenefits middleware, while also loggin the errors
    and returning 400 when it occurs.

--- node/middlewares/calculateExternalBenefits.ts
+++ node/middlewares/calculateExternalBenefits.ts
@@ -6,21 +6,47 @@ const calculateExternalBenefits = async (
   ctx: Context,
   next: () => Promise<any>
 ) => {
-  const simulationRequestBody = await json(ctx.req)
+  try {
+    const simulationRequestBody = await json(ctx.req)
 
-  ctx.state.parsedRequestProtocol = parseOrderFormToProtocol(
-    ctx.state.orderForm ?? simulationRequestBody
-  )
+    ctx.state.parsedRequestProtocol = parseOrderFormToProtocol(
+      ctx.state.orderForm ?? simulationRequestBody
+    )
 
-  ctx.state.externalProviderResponse = await ctx.clients.externalProvider.getBenefits(
-    ctx.state.parsedRequestProtocol
-  )
+    ctx.state.externalProviderResponse = await ctx.clients.externalProvider.getBenefits(
+      ctx.state.parsedRequestProtocol
+    )
+  } catch (err) {
+    ctx.vtex.logger.error({
+      calculateExternalBenefits: {
+        status: 'failed',
+        content: err.message,
+      },
+    })
+
+    ctx.status = 400
+    ctx.body = {
+      error: err.message,
+    }
+
+    return
+  }
 
   if (ctx.vtex.route.id === 'simulation') {
     ctx.body = ctx.state.externalProviderResponse
 
     return
   }
+
+  ctx.vtex.logger.info({
+    calculateExternalBenefits: {
+      status: 'External endpoint returned',
+      content: {
+        orderFormId: ctx.state.orderFormId,
+        externalEndpointResponse: ctx.state.externalProviderResponse,
+      },
+    },
+  })
 
   await next()
 }
