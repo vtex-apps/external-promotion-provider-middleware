13a49bf590281b13d5dc0af362fc8969eee2bed6
Pedro Cruz <pscruzzz@outlook.com>
2021-07-26 22:41:07 -0300

    feat: add error handling for missing items array and missing item id

--- node/utils/parseOrderFormToProtocol.ts
+++ node/utils/parseOrderFormToProtocol.ts
@@ -9,39 +9,49 @@ export function parseOrderFormToProtocol({
   marketingData,
   paymentData,
 }: CheckoutOrderForm): ExternalPromotionsRequestProtocol {
-  const itemsNonVariableFields = items.map((item) => {
+  if (!items) {
+    throw new Error('Property "items" is obligatory')
+  }
+
+  const itemsNonVariableFields = items.map((item, index) => {
+    if (!item.id) {
+      throw new Error(
+        `Property "id" is missing in index ${index} inside Items Array (this property is obligatory)`
+      )
+    }
+
     return {
       id: item.id,
-      productId: item.productId,
-      refId: item.refId,
-      ean: item.ean,
-      name: item.name,
-      skuName: item.skuName,
-      modalType: item.modalType,
-      productCategoryIds: item.productCategoryIds,
-      productCategories: item.productCategories,
-      availability: item.availability,
-      measurementUnit: item.measurementUnit,
+      productId: item?.productId,
+      refId: item?.refId,
+      ean: item?.ean,
+      name: item?.name,
+      skuName: item?.skuName,
+      modalType: item?.modalType,
+      productCategoryIds: item?.productCategoryIds,
+      productCategories: item?.productCategories,
+      availability: item?.availability,
+      measurementUnit: item?.measurementUnit,
     }
   })
 
   const itemsVariableFields = items.map((item) => {
     return {
-      assemblies: item.assemblies,
-      tax: item.tax,
-      price: item.price,
-      listPrice: item.listPrice,
-      manualPrice: item.manualPrice,
-      sellingPrice: item.sellingPrice,
-      isGift: item.isGift,
-      quantity: item.quantity,
-      attachments: item.attachments,
-      attachmentOfferings: item.attachmentOfferings,
-      offerings: item.offerings,
-      priceTags: item.priceTags,
-      unitMultiplier: item.unitMultiplier,
-      seller: item.seller,
-      sellerChain: item.sellerChain,
+      assemblies: item?.assemblies,
+      tax: item?.tax,
+      price: item?.price,
+      listPrice: item?.listPrice,
+      manualPrice: item?.manualPrice,
+      sellingPrice: item?.sellingPrice,
+      isGift: item?.isGift,
+      quantity: item?.quantity,
+      attachments: item?.attachments,
+      attachmentOfferings: item?.attachmentOfferings,
+      offerings: item?.offerings,
+      priceTags: item?.priceTags,
+      unitMultiplier: item?.unitMultiplier,
+      seller: item?.seller,
+      sellerChain: item?.sellerChain,
     }
   })
 
