d89a9f74fa83e7e7970bb469318603d0c54235d0
Pedro Cruz <pscruzzz@outlook.com>
2021-07-27 13:44:19 -0300

    refactor: refact nonVariableFields and variableFields loop to be more efficient

--- node/utils/parseOrderFormToProtocol.ts
+++ node/utils/parseOrderFormToProtocol.ts
@@ -1,5 +1,43 @@
-import type { CheckoutOrderForm } from '../typings/global'
+import type {
+  CheckoutOrderForm,
+  Attachment,
+  AttachmentOffering,
+  PriceTag,
+} from '../typings/global'
 import type { ExternalPromotionsRequestProtocol } from '../typings/protocol/request'
+
+interface INonVariableFields {
+  id: string
+  productId: string
+  refId: string
+  ean: string
+  name: string
+  skuName: string
+  modalType: any
+  productCategoryIds: string
+  productCategories: { [key: string]: string }
+  availability: string
+  measurementUnit: string
+}
+
+interface IVariableFields {
+  assemblies: any[]
+  tax: number
+  price: number
+  listPrice: number
+  manualPrice: number | undefined
+  sellingPrice: number
+  isGift: boolean
+  quantity: number
+  attachments: Attachment[]
+  attachmentOfferings: AttachmentOffering[]
+  offerings: any[]
+  priceTags: PriceTag[]
+  unitMultiplier: number
+  seller: string
+  sellerChain: string[]
+}
+;[]
 
 export function parseOrderFormToProtocol({
   items,
@@ -13,47 +51,57 @@ export function parseOrderFormToProtocol({
     throw new Error('Property "items" is obligatory')
   }
 
-  const itemsNonVariableFields = items.map((item, index) => {
-    if (!item.id) {
-      throw new Error(
-        `Property "id" is missing in index ${index} inside Items Array (this property is obligatory)`
-      )
-    }
+  const [itemsVariableFields, itemsNonVariableFields] = items.reduce(
+    (
+      [variableFields, nonVariableFields]: [
+        IVariableFields[],
+        INonVariableFields[]
+      ],
+      item,
+      index
+    ) => {
+      if (!item.id) {
+        throw new Error(
+          `Property "id" is missing in index ${index} inside Items Array (this property is obligatory)`
+        )
+      }
 
-    return {
-      id: item.id,
-      productId: item?.productId,
-      refId: item?.refId,
-      ean: item?.ean,
-      name: item?.name,
-      skuName: item?.skuName,
-      modalType: item?.modalType,
-      productCategoryIds: item?.productCategoryIds,
-      productCategories: item?.productCategories,
-      availability: item?.availability,
-      measurementUnit: item?.measurementUnit,
-    }
-  })
+      nonVariableFields.push({
+        id: item.id,
+        productId: item?.productId,
+        refId: item?.refId,
+        ean: item?.ean,
+        name: item?.name,
+        skuName: item?.skuName,
+        modalType: item?.modalType,
+        productCategoryIds: item?.productCategoryIds,
+        productCategories: item?.productCategories,
+        availability: item?.availability,
+        measurementUnit: item?.measurementUnit,
+      })
 
-  const itemsVariableFields = items.map((item) => {
-    return {
-      assemblies: item?.assemblies,
-      tax: item?.tax,
-      price: item?.price,
-      listPrice: item?.listPrice,
-      manualPrice: item?.manualPrice,
-      sellingPrice: item?.sellingPrice,
-      isGift: item?.isGift,
-      quantity: item?.quantity,
-      attachments: item?.attachments,
-      attachmentOfferings: item?.attachmentOfferings,
-      offerings: item?.offerings,
-      priceTags: item?.priceTags,
-      unitMultiplier: item?.unitMultiplier,
-      seller: item?.seller,
-      sellerChain: item?.sellerChain,
-    }
-  })
+      variableFields.push({
+        assemblies: item?.assemblies,
+        tax: item?.tax,
+        price: item?.price,
+        listPrice: item?.listPrice,
+        manualPrice: item?.manualPrice,
+        sellingPrice: item?.sellingPrice,
+        isGift: item?.isGift,
+        quantity: item?.quantity,
+        attachments: item?.attachments,
+        attachmentOfferings: item?.attachmentOfferings,
+        offerings: item?.offerings,
+        priceTags: item?.priceTags,
+        unitMultiplier: item?.unitMultiplier,
+        seller: item?.seller,
+        sellerChain: item?.sellerChain,
+      })
+
+      return [variableFields, nonVariableFields]
+    },
+    [[], []]
+  )
 
   const formattedItems = itemsNonVariableFields.reduce(
     (memo: any[], itemNonVariableFields, index) => {
