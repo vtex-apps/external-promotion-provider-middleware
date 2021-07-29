59439cd076245bff785fbcfbb3fc309c0246af50
Pedro Cruz <pscruzzz@outlook.com>
2021-07-29 20:16:08 -0300

    feat: add error handling for when items array is falsy

--- node/utils/validateIndexConsistency.ts
+++ node/utils/validateIndexConsistency.ts
@@ -1,15 +1,25 @@
 import type { CheckoutOrderForm } from '../typings/global'
 import type { ExternalPromotionsResponseProtocol } from '../typings/protocol/response'
 
-export function validateIndexConsistency(
+export async function validateIndexConsistency(
   orderForm: CheckoutOrderForm,
   providerResponse: ExternalPromotionsResponseProtocol
 ) {
   const { items: orderFormItems } = orderForm
   const { items } = providerResponse
 
+  if (!items) {
+    throw new Error(
+      `Items array is obligatory. If no promotions are going to be applied, this property should be sent as an empty array`
+    )
+  }
+
   items.forEach((item) => {
     const { id, variations } = item
+
+    if (!variations) {
+      throw new Error(`Variations array is missing in skuId ${id}.`)
+    }
 
     variations.forEach((variation) => {
       const { requestIndex } = variation
