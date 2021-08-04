f815062eccd75d2d160f5ee20904e7cf7923ffe7
Pedro Cruz <pscruzzz@outlook.com>
2021-08-04 00:24:07 -0300

    test: add new test to parseOrderFormToProtocol util

--- node/__tests__/unit/utils/parseOrderFormToProtocol.spec.ts
+++ node/__tests__/unit/utils/parseOrderFormToProtocol.spec.ts
@@ -9,4 +9,26 @@ describe('parseOrderFormToProtocol', () => {
       parsedProtocol
     )
   })
+  it('should be able to throw error when payload has no items array', async () => {
+    expect(() => {
+      parseOrderFormToProtocol({
+        customData: {},
+        paymentData: { payment: '' },
+      } as any)
+    }).toThrowError('Property "items" is obligatory')
+  })
+  it('should be able to throw error when items array has no skuId', async () => {
+    expect(() => {
+      parseOrderFormToProtocol({
+        items: [
+          { id: 33, otherfield: '' },
+          { id: 23, otherfield: '' },
+          { otherfield: '' },
+        ],
+        customData: {},
+      } as any)
+    }).toThrowError(
+      `Property "id" is missing in index 2 inside Items Array (this property is obligatory)`
+    )
+  })
 })
